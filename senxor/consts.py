1d4a16a7d4704a206f955a80bc00c11f3d48dce7
Shui <zelongshui@meridianinno.com>
2025-07-11 17:08:19 +0800

    feat: Update SENXOR_TYPE and add MODULE_TYPE and MCU_TYPE constants in consts.py

--- senxor/consts.py
+++ senxor/consts.py
@@ -18,14 +18,27 @@ SENXOR_FRAME_SHAPE = {
 }
 
 
-# The type of the senxor.
 SENXOR_TYPE = {
     0: "MI0801-non-MP",
     1: "MI0801",
     2: "MI0802",
-    4: "MI0802-1",
-    5: "MI0802-2",
-    6: "MI1603",
+    4: "MI0802-Rev1",
+    5: "MI0802-Rev2",
+    6: "MI16XX-Rev1",
+}
+
+
+MODULE_TYPE = {
+    19: "MI0802-M5S",
+    20: "MI0802-M6S",
+    21: "MI0802-M7G",
+    22: "MI0802-50",
+    24: "MI0802-M230",
+    28: "MI1602-M5S",
+    29: "MI1602-M6C",
+    40: "MI1602-M5S",
+    41: "MI1602-M6C",
+    255: "MI0801",
 }
 
 # We can get the frame shape from the senxor type.
@@ -37,4 +50,13 @@ SENXOR_TYPE2FRAME_SHAPE = {
     4: (62, 80),
     5: (62, 80),
     6: (120, 160),
+}
+
+MCU_TYPE = {
+    0: "MI48D4",
+    1: "MI48D5",
+    2: "MI48E",
+    3: "MI48G",
+    4: "MI48C",
+    255: "MI48D4",
 }
