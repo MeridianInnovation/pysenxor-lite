2ecd32e2f440517891677071e775964e92a5d1f3
Shui <zelongshui@meridianinno.com>
2025-10-02 10:57:27 +0800

    refactor: Revise logging setup in senxor.log.py to enhance modularity and configurability.

--- senxor/log.py
+++ senxor/log.py
@@ -5,28 +5,43 @@
 from __future__ import annotations
 
 import logging
-import logging.config
+import logging.handlers
 from pathlib import Path
-from typing import TYPE_CHECKING, TextIO, cast
 
 import structlog
-
-if TYPE_CHECKING:
-    from structlog.types import FilteringBoundLogger
+from structlog.stdlib import BoundLogger
 
 __all__ = [
+    "SenxorLogger",
     "get_logger",
     "setup_console_logger",
     "setup_file_logger",
     "setup_standard_logger",
 ]
 
+SenxorLogger = BoundLogger
 
-def get_logger(*args, **kwargs) -> FilteringBoundLogger:
+DEFAULT_LOGGER_LEVEL = logging.WARNING
+LOGGER_PROCESSORS = [
+    structlog.stdlib.filter_by_level,
+    structlog.stdlib.PositionalArgumentsFormatter(),
+    structlog.processors.add_log_level,
+    structlog.stdlib.add_logger_name,
+    structlog.contextvars.merge_contextvars,
+    structlog.processors.TimeStamper("%Y-%m-%dT%H:%M:%S%z", utc=False),
+    structlog.processors.StackInfoRenderer(),
+    structlog.dev.set_exc_info,
+    structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+]
+
+
+def get_logger(logger_name: str = "senxor", *args, **kwargs) -> SenxorLogger:
     """Get a structured logger instance.
 
     Parameters
     ----------
+    logger_name : str, optional
+        The name of the logger, by default `"senxor"`
     *args : Any
         Positional arguments to pass to `structlog.get_logger`.
     **kwargs : Any
@@ -34,7 +49,7 @@ def get_logger(*args, **kwargs) -> FilteringBoundLogger:
 
     Returns
     -------
-    structlog.BoundLogger
+    SenxorLogger
         A structured logger instance.
 
     Examples
@@ -45,19 +60,8 @@ def get_logger(*args, **kwargs) -> FilteringBoundLogger:
     [warning  ] This is a warning message.    device_id=1
 
     """
-    return structlog.get_logger(*args, **kwargs)
-
-
-class _ConsoleOutPutProcessor:
-    def __init__(self):
-        self._formatter = structlog.dev.ConsoleRenderer(sort_keys=False)
-        self._writer = structlog.PrintLogger()
-
-    def __call__(self, logger, method, event):
-        ev = event.copy()
-        msg = self._formatter(logger, method, event)
-        self._writer.msg(msg)
-        return ev
+    logger: BoundLogger = structlog.get_logger(logger_name, *args, **kwargs)
+    return logger
 
 
 def setup_standard_logger():
@@ -71,24 +75,37 @@ def setup_standard_logger():
     Instead, use `setup_console_logger` or `setup_file_logger` to setup a
     logger for your application.
     """
-    processors = [
-        structlog.stdlib.filter_by_level,
-        structlog.contextvars.merge_contextvars,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
-        structlog.processors.StackInfoRenderer(),
-        structlog.processors.format_exc_info,
-        structlog.dev.ConsoleRenderer(sort_keys=False),
-    ]
     structlog.configure(
-        processors=processors,
+        processors=LOGGER_PROCESSORS,
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
 
+    formatter_processors = [
+        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+        structlog.dev.ConsoleRenderer(sort_keys=False),
+    ]
 
-def setup_console_logger(log_level: int = logging.INFO):
+    senxor_logger = logging.getLogger("senxor")
+    senxor_logger.setLevel(DEFAULT_LOGGER_LEVEL)
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processors=formatter_processors,
+    )
+
+    if senxor_logger.hasHandlers():
+        for handler_ in senxor_logger.handlers:
+            senxor_logger.removeHandler(handler_)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(DEFAULT_LOGGER_LEVEL)
+    handler.setFormatter(formatter)
+    senxor_logger.addHandler(handler)
+    senxor_logger.propagate = False
+
+
+def setup_console_logger(log_level: int | str = "INFO", logger_name: str = "senxor", *, add_logger_name: bool = False):
     """Setup a console logger.
 
     This logger is used to log messages to the console, can output colored
@@ -96,78 +113,122 @@ def setup_console_logger(log_level: int = logging.INFO):
 
     Parameters
     ----------
-    log_level : int, optional
+    log_level : int | str | None, optional
         The log level to use, by default `logging.INFO`
+    logger_name : str, optional
+        The name of the logger, by default `"senxor"`
+    add_logger_name : bool, optional
+        Whether to add the logger name to the console output, by default `False`
 
     """
-    structlog.configure(
-        processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.processors.add_log_level,
-            structlog.processors.StackInfoRenderer(),
-            structlog.dev.set_exc_info,
-            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-            structlog.dev.ConsoleRenderer(sort_keys=False),
-        ],
-        wrapper_class=structlog.make_filtering_bound_logger(log_level),
-        context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(),
-        cache_logger_on_first_use=True,
+    formatter_processors = [
+        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+        structlog.dev.ConsoleRenderer(sort_keys=False),
+    ]
+
+    if not add_logger_name:
+        formatter_processors.insert(0, _remove_logger_name_processor)
+
+    logger = logging.getLogger(logger_name)
+
+    handler_level = _get_log_level(log_level)
+    logger_level = logger.getEffectiveLevel()
+    logger_level = min(logger_level, handler_level)
+    logger.setLevel(logger_level)
+
+    if logger.hasHandlers():
+        for handler_ in logger.handlers:
+            logger.removeHandler(handler_)
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processors=formatter_processors,
     )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    handler.setLevel(handler_level)
+    logger.addHandler(handler)
+    logger.propagate = False
 
 
 def setup_file_logger(
-    path: Path | str,
-    log_level: int = logging.INFO,
+    file_path: Path | str,
+    file_mode: str = "w",
+    log_level: int | str = "INFO",
+    logger_name: str = "senxor",
     *,
-    file_mode: str = "wt+",
-    log_as_json: bool = False,
-    log_to_console: bool = True,
+    watch_file: bool = False,
+    json_format: bool = False,
 ):
     """Setup a file logger.
 
-    This logger is used to log messages to the console and a file, can output
-    formatted messages.
+    This logger is used to log messages to a file, can output formatted messages.
 
     Parameters
     ----------
-    path : Path | str
+    file_path : Path | str
         The path to the log file.
-    log_level : int, optional
-        The log level to use for the file, by default `logging.NOTSET`
     file_mode : str, optional
-        The mode to open the log file in, by default `"wt+"`
-    log_as_json : bool, optional
-        Whether to log to the file in JSON format, by default `False`
-    log_to_console : bool, optional
-        Whether to log to the console, by default `True`
+        The mode to use for the log file, by default `"w"`
+    log_level : int | str, optional
+        The log level to use, by default `logging.INFO`
+    logger_name : str, optional
+        The name of the logger, by default `"senxor"`
+    watch_file : bool, optional
+        Whether to use the `logging.handlers.WatchedFileHandler` handler, by default `False`
+    json_format : bool, optional
+        Whether to use the `structlog.processors.JSONRenderer` formatter, by default `False`
 
     """
-    file = cast("TextIO", Path(path).open(file_mode))  # noqa: SIM115
+    logger = logging.getLogger(logger_name)
+    handler_level = _get_log_level(log_level)
+    logger_level = logger.getEffectiveLevel()
+    logger_level = min(logger_level, handler_level)
+    logger.setLevel(logger_level)
 
-    processors = [
-        structlog.contextvars.merge_contextvars,
-        structlog.processors.add_log_level,
-        structlog.processors.StackInfoRenderer(),
-        structlog.dev.set_exc_info,
-        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-    ]
+    if logger.hasHandlers():
+        for handler_ in logger.handlers:
+            logger.removeHandler(handler_)
 
-    if log_to_console:
-        processors.append(_ConsoleOutPutProcessor())
+    file_path = Path(file_path)
 
-    if log_as_json:
-        processors.append(structlog.processors.JSONRenderer())
+    if watch_file:
+        handler = logging.handlers.WatchedFileHandler(file_path, mode=file_mode)
     else:
-        processors.append(structlog.dev.ConsoleRenderer(sort_keys=False, colors=False))
+        handler = logging.FileHandler(file_path, mode=file_mode)
 
-    structlog.configure(
-        processors=processors,
-        wrapper_class=structlog.make_filtering_bound_logger(log_level),
-        context_class=dict,
-        logger_factory=structlog.WriteLoggerFactory(file),
-        cache_logger_on_first_use=True,
+    if json_format:
+        formatter_processors = [
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.processors.JSONRenderer(),
+        ]
+    else:
+        formatter_processors = [
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            structlog.dev.ConsoleRenderer(sort_keys=False, colors=False),
+        ]
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        processors=formatter_processors,
     )
+    handler.setFormatter(formatter)
+    handler.setLevel(handler_level)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+
+def _remove_logger_name_processor(_, __, event: dict):
+    event.pop("logger", None)
+    return event
+
+
+def _get_log_level(level: int | str):
+    if isinstance(level, str):
+        log_level_ = logging.getLevelNamesMapping().get(level.upper())
+        if log_level_ is None:
+            raise KeyError(f"Invalid log level: {level}")
+        return log_level_
+    else:
+        return level
 
 
 setup_standard_logger()
