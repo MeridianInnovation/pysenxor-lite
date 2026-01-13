92fde596f6604140d1c285b03a4a072e1d7bb27d
Shui <zelongshui@meridianinno.com>
2026-01-13 14:56:02 +0800

    refactor: Remove old regmap module

--- senxor/regmap/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025 Meridian Innovation. All rights reserved.
-
-from senxor.regmap._field import Field
-from senxor.regmap._fields import Fields
-from senxor.regmap._reg import Register
-from senxor.regmap._regs import Registers
-
-__all__ = ["Field", "Fields", "Register", "Registers"]
